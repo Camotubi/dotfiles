92f34d48f7cca6b739c849a131a8657476f33da7
Carlos Moisés Tuñón Biddy <camotubi@gmail.com>
2018-04-23 06:10:42 -0500

    add a install to automate copy of files

---
+++ install.sh
@@ -0,0 +1,10 @@
+CONFIGDIR="$HOME/.config"
+
+#Neovim config
+cp nvim/init.vim $CONFIGDIR/nvim/init.vim
+#Dunst config
+cp dunst/dunstrc $CONFIGDIR/dunst/dunstrc
+#i3 config
+cp i3/config $CONFIGDIR/i3/config
+#compton
+cp compton/compton.conf $CONFIGDIR/compton.conf
